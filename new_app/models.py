86ab53d65511e814044b81a21afbac5e9c7825e4
ArthurWuTW <lion811004@gmail.com>
2021-02-23 11:26:24 +0800

    Add Product and Order Model

--- new_app/models.py
+++ new_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    product_id = models.IntegerField(blank=False, null=False)
+    stock_pcs = models.IntegerField(blank=False, null=False)
+    price = models.IntegerField(blank=False, null=False)
+    shop_id = models.CharField(max_length=100, null=False)
+    vip = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(product_id)
+
+    class Meta:
+        verbose_name_plural = 'Product'
+
+class Order(models.Model):
+    id = models.IntegerField(blank=False, null=False, primary_key=True)
+    product_id = models.IntegerField(blank=False, null=False)
+    qty = models.IntegerField(blank=False, null=False)
+    price = models.IntegerField(blank=False, null=False)
+    shop_id = models.CharField(max_length=100, null=False)
+
+    def __str__(self):
+        return str(id)
+
+    class Meta:
+        verbose_name_plural = 'Order'
