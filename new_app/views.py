e4667373c29b9dfb7ba87a88bd36b341acd16198
ArthurWuTW <lion811004@gmail.com>
2021-02-23 15:30:54 +0800

    frontend draft

--- new_app/views.py
+++ new_app/views.py
@@ -3,6 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 from django.views import View
 
+
+
 class Page(View):
     def get(self, request):
+        return render(self.request, 'page.html', {})
+
+    def post(self, request):
+
         return render(self.request, 'page.html', {})
