54c4a6718315c9d2304ca009d35b98d63bfdec04
ArthurWuTW <lion811004@gmail.com>
2021-02-25 09:34:02 +0800

    TODO - unit test

--- new_app/tests.py
+++ new_app/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import resolve
+from selenium import webdriver
+
 
 # Create your tests here.
+class ViewTest(TestCase):
+
+    def test_url_resloves_to_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func.view_class, new_app.views.Page)
+
+    # TODO: test_add, test_delete, test_top3
