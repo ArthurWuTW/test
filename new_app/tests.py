48bd136decb8fa7211b9f4ef4e176c41b31264f3
ArthurWuTW <lion811004@gmail.com>
2021-02-27 20:59:17 +0800

    bugfix selenium

--- new_app/tests.py
+++ new_app/tests.py
@@ -1,14 +1,27 @@
 from django.test import TestCase
 from django.urls import resolve
-from selenium import webdriver
 import new_app.views
-
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
 # Create your tests here.
 class ViewTest(TestCase):
 
     def test_url_resloves_to_view(self):
+
         found = resolve('/')
         self.assertEqual(found.func.view_class, new_app.views.Page)
+
+    def test_add_data(self):
+
+        options = Options()
+        options.headless = True
+        driver = webdriver.Firefox(options=options)
+
+        
+
+
+
+
 
     # TODO: test_add, test_delete, test_top3
