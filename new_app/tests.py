de2de4d58f9f76fdee1fe13cdffa2d6c681bc2b0
ArthurWuTW <lion811004@gmail.com>
2021-02-27 21:56:46 +0800

    selenium unit test

--- new_app/tests.py
+++ new_app/tests.py
@@ -1,8 +1,15 @@
+import sys
+import codecs
+sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
 from django.test import TestCase
 from django.urls import resolve
 import new_app.views
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.select import Select
+from django.test import Client
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
 
 # Create your tests here.
 class ViewTest(TestCase):
@@ -18,7 +25,30 @@ class ViewTest(TestCase):
         options.headless = True
         driver = webdriver.Firefox(options=options)
 
-        
+class MySeleniumTests(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+
+    def test_login(self):
+
+        options = Options()
+        options.headless = True
+        driver = webdriver.Firefox(options=options)
+        driver.get(self.live_server_url)
+        print(driver.page_source)
+
+
+
+
+
+
+
 
 
 
