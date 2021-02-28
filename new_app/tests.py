411bc4af23bd0d7c3d89a1649b5f38891245ab59
ArthurWuTW <lion811004@gmail.com>
2021-02-28 16:13:52 +0800

    test add data, test not-vip condition

--- new_app/tests.py
+++ new_app/tests.py
@@ -11,6 +11,7 @@ from django.test import Client
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.firefox.webdriver import WebDriver
 import os
+from new_app.models import Product, Order
 
 # Create your tests here.
 
@@ -35,7 +36,7 @@ class MySeleniumTests(StaticLiveServerTestCase):
             [6,	5,	60,	    'um',	False],
             [7,	5,	800,	'ps',	True]
         ]
-        from new_app.models import Product
+
         for product in products:
             new_data = Product()
             new_data.product_id = product[0]
@@ -83,35 +84,76 @@ class MySeleniumTests(StaticLiveServerTestCase):
         # Order every products
         for product in products:
             driver.get(self.live_server_url)
-            print(driver.page_source)
+
             # Select
             opt = driver.find_element_by_name('product_name')
             Select(opt).select_by_index(str(product[0]))
 
-            # insert custom name
+            # add order
             driver.find_element_by_id('product_number').send_keys(str(product[1]))
             driver.find_element_by_id('customer_id').send_keys('ABC')
             driver.find_element_by_id('isVIP').click()
             driver.find_element_by_id('image-btn').click()
 
-            print(driver.page_source)
-
             # check order
-
-
+            queried_order = Order.objects.latest('id')
+            self.assertEqual(queried_order.product_id, product[0])
+            self.assertEqual(queried_order.qty, product[1])
+            self.assertEqual(queried_order.price, product[2])
+            self.assertEqual(queried_order.shop_id, product[3])
+            self.assertEqual(queried_order.customer_id, 'ABC')
 
         driver.quit()
 
-            # Check database data
+    def test_vip_status(self):
+        products = [
+            [1,	6,	150,	'um',	False],
+            [2,	10,	110,	'ms',	False],
+            [3,	20,	900,	'ps',	False],
+            [4,	2,	1899,	'ps',	True],
+            [5,	8,	35,	    'ms',	False],
+            [6,	5,	60,	    'um',	False],
+            [7,	5,	800,	'ps',	True]
+        ]
 
-            # Check frontend html
+        for product in products:
+            new_data = Product()
+            new_data.product_id = product[0]
+            new_data.stock_pcs = product[1]
+            new_data.price = product[2]
+            new_data.shop_id  = product[3]
+            new_data.vip  = product[4]
+            new_data.save()
+
+        options = Options()
+        options.headless = True
+        driver = webdriver.Firefox(options=options)
+
+        for product in products:
+            if(product[4]==True):
+                driver.get(self.live_server_url)
+
+                print(driver.page_source)
+
+                # Select
+                opt = driver.find_element_by_name('product_name')
+                Select(opt).select_by_index(str(product[0]))
+
+                # add order
+
+                driver.find_element_by_id('product_number').send_keys(str(product[1]))
+                driver.find_element_by_id('customer_id').send_keys('ABC')
+                driver.find_element_by_id('image-btn').click()
+
+                self.assertIn('你不是ｖｉｐ', driver.page_source)
+
+    def test_understock_status(self):
+        a= 1
+    def test_stock_arrived_status(self):
 
 
-            # Clear d
 
-
-
-
+        a = 1
 
 
 
